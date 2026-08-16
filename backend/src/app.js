54decb357d76194b04d277b388dbccd272ec3a34
Ankit Nagar <iamankitnagar7@gmail.com>
2026-08-16 14:33:50 +0530

    Initial commit

---
+++ backend/src/app.js
@@ -0,0 +1,29 @@
+const express = require("express");
+const cookieParser = require("cookie-parser");
+const cors = require("cors");
+
+
+
+const app = express();  
+app.use(express.json());
+app.use(cookieParser());
+app.use(cors({
+  origin: [
+    "http://localhost:5173",
+    "http://localhost:3000",
+    "https://ai-interview-prep-9hspdcbjk-ankit-webtechs-projects.vercel.app",
+    "https://ai-interview-prep-ebon.vercel.app"
+  ],
+  credentials: true
+}));
+
+//// require all routes here
+const authRouter = require("./routes/auth.routes");
+const interviewRouter = require("./routes/interview.routes");
+
+app.use("/api/auth", authRouter);
+app.use("/api/interview", interviewRouter);
+
+
+
+module.exports = app;

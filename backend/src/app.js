c1b505271a26dcc3b52ca770097b2208b5918d43
Ankit Nagar <iamankitnagar7@gmail.com>
2026-08-20 13:19:08 +0530

    fix production cors

--- backend/src/app.js
+++ backend/src/app.js
@@ -11,6 +11,7 @@ app.use(cors({
   origin: [
     "http://localhost:5173",
     "http://localhost:3000",
+    "https://gen-ai-interview-ten.vercel.app",
     "https://ai-interview-prep-9hspdcbjk-ankit-webtechs-projects.vercel.app",
     "https://ai-interview-prep-ebon.vercel.app"
   ],
